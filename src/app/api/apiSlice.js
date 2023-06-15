513ff85c6bd2e45a2d963ccb1f743fa3cf2b078a
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2023-06-15 14:03:51 +0100

    update one

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://DLt-technote-api.onrender.com',
+  baseUrl: 'https://DLt-note-api.onrender.com',
   prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token
 
